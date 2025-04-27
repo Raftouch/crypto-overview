4510041d273be1a838899ef469d85af46333f962
raftouch <rafanelkin@gmail.com>
2025-04-27 18:54:46 +0200

    add select by currency

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -9,10 +9,11 @@ interface Coin {
 
 export default function MainPage() {
   const [coins, setCoins] = useState<Coin[]>([]);
+  const [currency, setCurrency] = useState("usd");
 
-  const fetchCoins = async () => {
+  const fetchCoins = async (currency: string) => {
     try {
-      const response = await api.get("/");
+      const response = await api.get(`/coins?currency=${currency}`);
       const data = response.data;
       console.log("data : ", data);
       setCoins(data);
@@ -22,20 +23,41 @@ export default function MainPage() {
   };
 
   useEffect(() => {
-    fetchCoins();
-  }, []);
+    fetchCoins(currency);
+  }, [currency]);
+
+  const handleCurrencyChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setCurrency(event.target.value);
+  };
 
   return (
-    <ul className="space-y-2 text-sm">
-      {coins.map((coin) => (
-        <li
-          key={coin.id}
-          className="flex justify-between items-center p-3 bg-gray-100 rounded-lg shadow-md hover:bg-gray-200 transition-colors duration-300"
-        >
-          <span className="font-semibold text-gray-800">{coin.symbol}</span>
-          <span className="text-gray-600">{coin.current_price}</span>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <select
+        className="mb-4 p-2 rounded"
+        value={currency}
+        onChange={handleCurrencyChange}
+      >
+        <option value="usd">USD - United States Dollar</option>
+        <option value="eur">EUR - Euro</option>
+        <option value="gbp">GBP - British Pound</option>
+        <option value="jpy">JPY - Japanese Yen</option>
+        <option value="btc">BTC - Bitcoin</option>
+        <option value="eth">ETH - Ethereum</option>
+      </select>
+
+      <ul className="space-y-2 text-sm">
+        {coins.map((coin) => (
+          <li
+            key={coin.id}
+            className="flex justify-between items-center p-3 bg-gray-100 rounded-lg shadow-md hover:bg-gray-200 transition-colors duration-300"
+          >
+            <span className="font-semibold text-gray-800">{coin.symbol}</span>
+            <span className="text-gray-600">{coin.current_price}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi import Query
 from fastapi.middleware.cors import CORSMiddleware
 import httpx
 
@@ -17,15 +18,26 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-BINANCE_API_URL = "https://api.binance.com/api/v3/ticker/price"
+# BINANCE_API_URL = "https://api.binance.com/api/v3/ticker/price"
+API_URL = "https://api.coingecko.com/api/v3/coins/markets"
 
-@app.get("/")
-async def get_coins():
+
+async def get_coins_data(currency: str):
     async with httpx.AsyncClient() as client:
-        response = await client.get(BINANCE_API_URL)
-        coins = response.json()
+        response = await client.get(f"{API_URL}?vs_currency={currency}")
+        if response.status_code != 200:
+            return {"error": "Error fetching coins"}
+        return response.json()
+
+
+@app.get("/coins")
+async def get_coins(vs_currency: str = Query("usd", alias="currency")):
+    try:
+        coins = await get_coins_data(vs_currency)
         sorted_coins = sorted(coins, key=lambda coin: coin["symbol"])
         return sorted_coins
+    except TypeError as error:
+        return {"error": "Wrong format"}
 
 
 if __name__ == "__main__":
